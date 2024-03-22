a5a2864256aa06b1d9acfe4a41cabbfcf6bc67c2
Saishree L <142288119+Saishree9@users.noreply.github.com>
2024-03-22 18:57:12 +0530

    folder
    need to make changes in table filteration and footer buttons

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+// -------------------------teams------------------------------
 const teams = [
     {
         icon: `<i class="fa fa-pencil" aria-hidden="true"></i>`,
@@ -23,22 +25,171 @@ function navBar() {
 }
 navBar();
 
-// function addMore() {
-//     const newTeam = { icon: 'new_team_icon.png', team: 'New Team' };
-//     teams.push(newTeam);
-//     populateDivWithTeams(); // Re-populate the div with updated teams
-// }
+// -------------------------products------------------------------
+
+const products = ["Roadmap", "Feedback", "Performance", "Team", "Analytics"]
+
+function productsList() {
+    const folder = document.getElementById('products');
+    const listItems = products.map(folder => {
+        return `<li>${folder}</li>`;
+    });
+    const btn = () => {
+        return `<li class="button"><i class="fa fa-plus-square-o" aria-hidden="true"></i> Add New Folder</li>`
+    }
+    folder.innerHTML = `<ul>${listItems.join('')} ${btn()} </ul>`
+}
+
+// -------------------------sales------------------------------
+
+const sales = ["sales 1", "sales 2"]
+
+function salesList() {
+    const folder = document.getElementById('sales')
+    const listItems = sales.map(sale => {
+        return `<li>${sale}</li>`;
+    });
+    const btn = () => {
+        return `<li class="button"><i class="fa fa-plus-square-o" aria-hidden="true"></i> Add New Folder</li>`
+    }
+    folder.innerHTML = `<ul>${listItems.join('')} ${btn()} </ul>`
+}
 
 
-const folders = ["Products", "Sales","Design", "Office", "Legal"]
+// -------------------------notes------------------------------
 
-function foldersList() {
-    const folder = document.getElementById('folder');
-    const listItems = folders.map(folder => {
-        return `<li class="folderList"><i class="fa fa-folder-o" aria-hidden="true"></i> ${folder}</li>`;
+const note = [
+    {
+        icon: `<i class="fa fa-users" aria-hidden="true"></i>`,
+        teamName: "Invite teammates"
+    },
+    {
+        icon: `<i class="fa fa-info-circle" aria-hidden="true"></i>`,
+        teamName: "Help and first steps"
+    },
+    {
+        icon: ``,
+        teamName: "7 days left on Trial"
+    },
+];
+
+function noteBar() {
+    const noteBar = document.getElementById('noteBar');
+    const listItems = note.map(team => {
+        return `<li class="teamList">${team.icon} ${team.teamName}</li>`;
 
     });
 
-    folder.innerHTML = `<ul>${listItems.join('')}</ul>`;
+    noteBar.innerHTML = `<ul>${listItems.join('')}</ul>`;
 }
-foldersList();
+noteBar()
+
+const table = [
+    {
+        checkbox: `<input type="checkbox" />`,
+        BrandLogo:`./images/wix.png`,
+        Brand: `Wix`,
+        Description: `Develop a personalised fit...`,
+        Members: `<i class="fa fa-user" aria-hidden="true"></i>`,
+        Categories: `Automation`,
+        Tags: `#digitalTransformation`,
+        Next_meeting: `in 30 minutes`,
+    },
+    {
+        checkbox: `<input type="checkbox" />`,
+        BrandLogo:`./images/shopify.png`,
+        Brand: `Shopify`,
+        Description: `Lorem ipsum, dolor sit amet ...`,
+        Members: `<i class="fa fa-user" aria-hidden="true"></i>`,
+        Categories: `E-commerce`,
+        Tags: `#digitalTransformation`,
+        Next_meeting: `in 10 secs`,
+    },
+    {
+        checkbox: `<input type="checkbox" />`,
+        BrandLogo:`./images/download.png`,
+        Brand: `MailChimp`,
+        Description: `consectetur adipisicing elit...`,
+        Members: `<i class="fa fa-user" aria-hidden="true"></i>`,
+        Categories: `SAAS`,
+        Tags: `#digitalTransformation`,
+        Next_meeting: `in 30 minutes`,
+    },
+    {
+        checkbox: `<input type="checkbox" />`,
+        BrandLogo:`./images/paypal.png`,
+        Brand: `Paypal`,
+        Description: `Placeat beatae dolorem...`,
+        Members: `<i class="fa fa-user" aria-hidden="true"></i>`,
+        Categories: `Mobile`,
+        Tags: `#digitalTransformation`,
+        Next_meeting: `in 24 hours`,
+    },
+    {
+        checkbox: `<input type="checkbox" />`,
+        BrandLogo:`./images/disney.jpg`,
+        Brand: `Disney`,
+        Description: `iusto maxime repellendus...`,
+        Members: `<i class="fa fa-user" aria-hidden="true"></i>`,
+        Categories: `Entertainment`,
+        Tags: `#digitalTransformation`,
+        Next_meeting: `in 30 minutes`,
+    },
+    {
+        checkbox: `<input type="checkbox" />`,
+        BrandLogo:`./images/google.png`,
+        Brand: `Google`,
+        Description: `magni sapiente nobis ame...`,
+        Members: `<i class="fa fa-user" aria-hidden="true"></i>`,
+        Categories: `technology`,
+        Tags: `#digitalTransformation`,
+        Next_meeting: `No contact`,
+    },
+    {
+        checkbox: `<input type="checkbox" />`,
+        BrandLogo:`./images/download.png`,
+        Brand: `MailChimp`,
+        Description: `consectetur adipisicing elit...`,
+        Members: `<i class="fa fa-user" aria-hidden="true"></i>`,
+        Categories: `Publishing`,
+        Tags: `#digitalTransformation`,
+        Next_meeting: `in 30 minutes`,
+    },
+    {
+        checkbox: `<input type="checkbox" />`,
+        BrandLogo:`./images/paypal.png`,
+        Brand: `Paypal`,
+        Description: `Placeat beatae dolorem...`,
+        Members: `<i class="fa fa-user" aria-hidden="true"></i>`,
+        Categories: `Transaction`,
+        Tags: `#digitalTransformation`,
+        Next_meeting: `in 6 hours`,
+    },
+    {
+        checkbox: `<input type="checkbox" />`,
+        BrandLogo:`./images/disney.jpg`,
+        Brand: `Disney`,
+        Description: `iusto maxime repellendus...`,
+        Members: `<i class="fa fa-user" aria-hidden="true"></i>`,
+        Categories: `Web service`,
+        Tags: `#digitalTransformation`,
+        Next_meeting: `No contact`,
+    },
+]
+
+function tableContent() {
+    let content = document.getElementById('content')
+    let tableItem = table.map((tab) => {
+        return `<tr>
+        <td style="width: 5%; text-align: center;">${tab.checkbox}</td>
+        <td><img src="${tab.BrandLogo}" class="tabImg"> ${tab.Brand}</td>
+        <td>${tab.Description}</td>
+        <td style="width: 5%; text-align: center;">${tab.Members}</td>
+        <td><button>${tab.Categories}</button></td>
+        <td><button  class="greyText">${tab.Tags}</button></td>
+        <td><button>${tab.Next_meeting}</button></td>
+      </tr>`
+    })
+    content.innerHTML = `${tableItem.join('')}`
+}
+tableContent()
